717af8c0c3c08aa546ffd0d73f54883f8372648b
akibhasan50 <tonuakibhasan@gmail.com>
2022-02-13 22:00:10 +0600

    express crud

---
+++ routeHandler/todoHandler.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const todoSchema = require("../schemas/todoSchema");
+const Todo = new mongoose.model("todo",todoSchema)
+// GET ALL THE TODOS
+router.get("/", async (req, res) => {
+  await Todo.find({status: "active"},(err,data) =>{
+    if(err) {
+        res.status(500).json({
+            error:"There is a serverside error"
+        })
+    }else{
+      res.status(200).json({
+          result: data,
+          message:"Todo get successfully"
+      })
+    }
+});
+});
+
+// GET A TODO by ID
+router.get("/:id", async (req, res) => {
+    await Todo.find({_id: "61a2689dd9592c271cecc027"},(err,data) =>{
+        if(err) {
+            res.status(500).json({
+                error:"There is a serverside error"
+            })
+        }else{
+          res.status(200).json({
+              result: data,
+              message:"Todo get successfully"
+          })
+        }
+    });
+});
+
+// POST A TODO
+router.post("/", async (req, res) => {
+  const newTodo = new Todo(req.body)
+  await newTodo.save((err) =>{
+      if(err) {
+          res.status(500).json({
+              error:"There is a serverside error"
+          })
+      }else{
+        res.status(200).json({
+            message:"Todo was inserted successfully"
+        })
+      }
+  }) 
+});
+
+// POST MULTIPLE TODO
+router.post("/all", async (req, res) => {
+    Todo.insertMany(req.body, (err)=>{
+        if(err) {
+            res.status(500).json({
+                error:"There is a serverside error"
+            })
+        }else{
+          res.status(200).json({
+              message:"Todo was inserted successfully"
+          })
+        }
+    })
+});
+
+// PUT TODO
+router.put("/:id", async (req, res) => {
+ const newdata = await Todo.findByIdAndUpdate({_id:req.params.id},{
+      $set:{
+          status:"inactive",
+          useFindAndModify:false
+      }
+  },{
+      new:true
+  },(err)=>{
+    if(err) {
+        res.status(500).json({
+            error:"There is a serverside error"
+        })
+    }else{
+      res.status(200).json({
+          message:"Todo was updated successfully"
+      })
+    }
+})
+
+console.log(newdata)
+});
+
+// DELETE TODO
+router.delete("/:id", async (req, res) => {
+    await Todo.deleteOne({_id: "61a2689dd9592c271cecc027"},(err) =>{
+        if(err) {
+            res.status(500).json({
+                error:"There is a serverside error"
+            })
+        }else{
+          res.status(200).json({
+              
+              message:"Todo deleted successfully"
+          })
+        }
+    });
+});
+
+module.exports = router;
